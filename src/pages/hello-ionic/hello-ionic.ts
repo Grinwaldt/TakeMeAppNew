043ebe136f12ab9381249987f3c395ca70a0b5a0
tziporag <tzipora.grinwald@tidetechnology.com>
2019-11-28 17:20:10 +0200

    images , tasks

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+// import { DocumentViewer } from '@ionic-native/document-viewer/ngx';
+import { NavController } from 'ionic-angular';
+
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  // private document: DocumentViewer,
+  constructor(public navCtrl: NavController) {
 
   }
+  GoToNewPage() {
+    this.navCtrl.push(ListPage);
+}
+  // const options: DocumentViewerOptions = {
+  //   title: 'My PDF'
+  // }
+  // this.document.viewDocument('../assets/documents/prayer.pdf', 'application/pdf', options)
 }
