3191393c537349b81e05a134e1e7b7f26649ebab
tziporag <tzipora.grinwald@tidetechnology.com>
2019-11-29 08:19:38 +0200

    video

--- src/pages/video/video.ts
+++ src/pages/video/video.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player/ngx';
-import { VideoPlayer } from '@ionic-native/video-player/ngx';
 import {DomSanitizer} from '@angular/platform-browser';
+import { TasksService } from '../../app/services/tasks.service';
 
-// import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
 /**
  * Generated class for the VideoPage page.
  *
@@ -19,26 +17,21 @@ import {DomSanitizer} from '@angular/platform-browser';
 })
 export class VideoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private youtube: YoutubeVideoPlayer,private videoPlayer: VideoPlayer,public  sanitizer:DomSanitizer) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public  sanitizer:DomSanitizer,
+    private tasksService: TasksService) {
   }
+
+  
+  
+  
 
   ionViewDidLoad() {
-//     this.videoPlayer.play('file:///android_asset/www/assets/SampleVideo.mp4').then(() => {
-// console.log('video completed');
-// }).catch(err => {
-// console.log(err);
-// });
-
-    const url="https://www.youtube.com/watch?v=DA0HAhgQCz4&list=PLBcT29Hg53cw7YhDXFSZ-WY1SLdZnjY07&index=10";
-    const id =this.getVideoIdFromUrl(url);
-    console.log('ionViewDidLoad VideoPage');
-    // this.youtube.openVideo('pveNYEim1Gw');
-    //this.youtube.openVideo('rhQmy93LZH8');
+    var vid = document.getElementById("myVideo"); 
+    vid.addEventListener('ended', (event) => {
+      console.log('Video stopped either because 1) it was over, ' +
+          'or 2) no further data is available.');
+    });
 
   }
-  private getVideoIdFromUrl(videoUrl){
-    const re = /^(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i;
-    const id = videoUrl.match(re)[7];
-    return id;
-  }
+ 
 }
